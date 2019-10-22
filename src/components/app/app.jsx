5f007ba5412f55300953839c0cd1be1bede9e31e
TM <spring.miracle@gmail.com>
2019-10-23 00:53:21 +0300

    Добавлен компонент Property

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
+import {Property} from '../property/property.jsx';
+
+const settings = {
+  title: "Cosy room in spacious apartment",
+  type: "Room",
+  price: 128,
+};
+
 const App = () => {
-  return <section className="welcome">
-    <h1 className="welcome__сities-title">6 Cities</h1>
-    <ul className="welcome__menu">
-      <li>Paris</li>
-      <li>Cologne</li>
-      <li>Brussels</li>
-      <li>Amsterdam</li>
-      <li>Humburg</li>
-      <li>Dusseldorf</li>
-    </ul>
-    <input className="welcome__search-input"/>
-    <button className="welcome__button"><span className="">Найти</span></button>
-    <p className="welcome__text">Удачи!</p>
-  </section>;
+  return <Property
+    title={settings.title}
+    type={settings.type}
+    price={settings.price}
+  />;
 };
 
 export {App};
