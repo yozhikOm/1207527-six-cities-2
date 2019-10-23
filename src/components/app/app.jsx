211c33111fd284875958f13a628713e14e253569
TM <spring.miracle@gmail.com>
2019-10-24 01:10:51 +0300

    Добавлен компонент Properties. Компоненту передается через props массив со списком объектов недвижимости

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import {Property} from '../property/property.jsx';
 
-const settings = {
-  title: "Cosy room in spacious apartment",
-  type: "Room",
-  price: 128,
-};
+import {Properties} from '../properties/properties.jsx';
+
+const properties = [
+  {
+    "id": 1,
+    "title": "Cozy house amsterdam",
+    "type": "House",
+    "price": 650,
+  },
+  {
+    "id": 2,
+    "title": "Cosy room in spacious apartment",
+    "type": "Room",
+    "price": 45,
+  },
+  {
+    "id": 3,
+    "title": "Elegant private room in stylish house",
+    "type": "Room",
+    "price": 128,
+  },
+  {
+    "id": 4,
+    "title": "Huge Tinyhouse in the green, between sea and city",
+    "type": "House",
+    "price": 1520,
+  }
+];
 
 const App = () => {
-  return <Property
-    title={settings.title}
-    type={settings.type}
-    price={settings.price}
-  />;
-};
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      <Properties items={properties} />
+    </div>
+  )
+}
 
 export {App};
---
+++ src/components/properties/properties.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Properties = (props) => {
+  return (
+    <React.Fragment>
+    {props.items.map(item => (
+        <React.Fragment key={item.id}>
+        <article className="cities__place-card place-card">
+          <div className="place-card__mark">
+            <span>Premium</span>
+          </div>
+          <div className="cities__image-wrapper place-card__image-wrapper">
+            <a href="#">
+                <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+            </a>
+          </div>
+          <div className="place-card__info">
+            <div className="place-card__price-wrapper">
+              <div className="place-card__price">
+                <b className="place-card__price-value">&euro;{item.price}</b>
+                <span className="place-card__price-text">&#47;&nbsp;night</span>
+              </div>
+              <button className="place-card__bookmark-button button" type="button">
+                <span className="visually-hidden">To bookmarks</span>
+              </button>
+            </div>
+            <div className="place-card__rating rating">
+              <div className="place-card__stars rating__stars">
+                
+                <span className="visually-hidden">Rating</span>
+              </div>
+            </div>
+            <h2 className="place-card__name">
+              <a href="#">{item.title}</a>
+            </h2>
+            <p className="place-card__type">{item.type}</p>
+          </div>
+        </article>
+        </React.Fragment>
+    ))}
+    </React.Fragment>
+  )
+}
+
+Properties.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+    }).isRequired
+  ).isRequired
+}
+
+export {Properties};
